026e22e8e1a0233289d56811b49df22f206c37e3
Adam Sunderman <amsunderman@gmail.com>
2023-01-07 19:37:51 -0600

    switching to shell scripting (requires a bash compatible shell like gitbash)

--- scripts/install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-call variables.cmd
-
-rem install scripts
-rmdir "%gamePath%\mods\%modName%\content\scripts" /s /q
-XCOPY "%modPath%\src" "%gamePath%\mods\%modName%\content\scripts\" /e /s /y
-XCOPY "%modPath%\strings" "%gamepath%\mods\%modname%\content\" /e /s /y
-copy "%modPath%\mod-menu.xml" "%gamePath%\bin\config\r4game\user_config_matrix\pc\%modname%.xml" /y
